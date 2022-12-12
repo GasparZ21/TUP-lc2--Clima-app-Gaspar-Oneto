3bf0e31343609c54d5741a7c4f8b53f764ed58fd
Gaspar Oneto <82554329+GasparZ21@users.noreply.github.com>
2022-12-12 19:44:58 -0300

    tp clima css

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
+const consultar = () =>{
+    let ciudad = document.querySelector("#ciudad").value;
+    console.log(ciudad)
 
-function agregar_ciudad(NuevaCiudad){
-    var NuevaCiudad = document.getElementById("agregar-ciudad").value;
-
-    if(NuevaCiudad == true){
-        alert("ciudad agregada!!");
-    }
-}
-
-function redirect(NuevaCiudad){
-    window.location.replace("http://127.0.0.1:5500/")
-}
+};
